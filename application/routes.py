051de864a0320aec12fd9a0cd2705434b7ca2806
Jeanette <khojeanette@gmail.com>
2024-03-29 22:36:24 +0000

    Added jinja for loop for tea images with temporary tea_list dictionary

--- application/routes.py
+++ application/routes.py
@@ -13,9 +13,16 @@ def home():
 # def about():
 
 
+tea_list = {'Strawberry': 'images/strawberry.webp', 'Brown Sugar': 'images/strawberry.webp',
+            'Taro': 'images/strawberry.webp', 'Matcha': 'images/strawberry.webp', 'Rose Milk':
+                'images/strawberry.webp', 'Mango Boba': 'images/strawberry.webp',
+            'Classic Milk': 'images/strawberry.webp', 'Honey Dew': 'images/strawberry.webp'}
+
+
 @app.route('/order')
 def order():
-    return render_template('order.html', active_order='active')
+    return render_template('order.html', active_order='active', tea_list=tea_list)
+
 
 # @app.route('/stores')
 # def our_stores():
