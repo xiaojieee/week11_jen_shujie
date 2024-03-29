36d8052a82a217648c2b3601fd3edf5055e9a18b
Jeanette <khojeanette@gmail.com>
2024-03-29 20:54:22 +0000

    Added top menu styling and highlighted link when on a page to routes

--- application/routes.py
+++ application/routes.py
@@ -6,17 +6,16 @@ from application import app
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', active_home='active')
 
 
-#
 # @app.route('/about')
 # def about():
-#
-#
+
+
 @app.route('/order')
 def order():
-    return render_template('order.html')
+    return render_template('order.html', active_order='active')
 
 # @app.route('/stores')
 # def our_stores():
