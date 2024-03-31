f958359b6398c9d01e65da43b23e1446fcf206fc
Jeanette <khojeanette@gmail.com>
2024-03-31 14:45:41 +0100

    Added initial basket page and route

--- application/routes.py
+++ application/routes.py
@@ -28,3 +28,7 @@ def order():
 @app.route('/stores/')
 def our_stores():
     return render_template('store.html', active_stores='active')
+
+@app.route('/basket/')
+def basket():
+    return render_template('basket.html', active_basket='active')
