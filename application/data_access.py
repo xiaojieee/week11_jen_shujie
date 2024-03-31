1f2593e81874f9eedb5c00862b061400c1357ef0
xiaojieee <s.j.zhu@outlook.com>
2024-03-31 15:51:59 +0100

    added database function

--- application/data_access.py
+++ application/data_access.py
@@ -1,1 +1,31 @@
 import _mysql_connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Pa$$w0rd",  # blank for mac
+  database="teajinja_db"
+)
+
+
+def get_db_connection():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Pa$$w0rd",  # blank for mac
+        database="teajinja_db"
+    )
+
+    return mydb
+
+
+def add_order(tea, user, collection):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO orders (tea_id, user_id, collection_time) VALUES (%s, %s, %s)"
+    val = (tea, user, collection)
+    cursor.execute(sql, val)
+
+    conn.commit()
+
